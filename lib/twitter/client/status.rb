e7cbccdcd925484e6e7edeaa3ad543323d6c57c4
John Daniels <john@semantici.st>
2009-03-21 14:22:09 +0000

    Adding support for setting in_reply_to_status_id when posting a new status.

--- lib/twitter/client/status.rb
+++ lib/twitter/client/status.rb
@@ -15,19 +15,22 @@ class Twitter::Client
   # <tt>value</tt> should be set to:
   # * the status identifier for <tt>:get</tt> case
   # * the status text message for <tt>:post</tt> case
-  # * none necessary for <tt>:delete</tt> case
+  # * the status identifier for <tt>:delete</tt> case
+  #
+  # <tt>reply_id</tt> is optional and used for <tt>:post</tt> actions only. It should be set to the status id of the status this is a reply to.
   # 
   # Examples:
   #  twitter.status(:get, 107786772)
   #  twitter.status(:post, "New Ruby open source project Twitter4R version 0.2.0 released.")
   #  twitter.status(:delete, 107790712)
+  #  twitter.status(:post "@twitter4r This rocks!", 107786772)
   # 
   # An <tt>ArgumentError</tt> will be raised if an invalid <tt>action</tt> 
   # is given.  Valid actions are:
   # * +:get+
   # * +:post+
   # * +:delete+
-  def status(action, value = nil)
+  def status(action, value = nil, reply_id = nil)
     return self.timeline_for(action, value || {}) if :replies == action
     raise ArgumentError, "Invalid status action: #{action}" unless @@STATUS_URIS.keys.member?(action)
     return nil unless value
@@ -37,7 +40,7 @@ class Twitter::Client
     when :get
     	response = http_connect {|conn|	create_http_get_request(uri, :id => value.to_i) }
     when :post
-    	response = http_connect({:status => value, :source => @@config.source}.to_http_str) {|conn| create_http_post_request(uri) }
+    	response = http_connect({:status => value, :source => @@config.source, :in_reply_to_status_id => reply_id}.to_http_str) {|conn| create_http_post_request(uri) }
     when :delete
     	response = http_connect {|conn| create_http_delete_request(uri, :id => value.to_i) }
     end
